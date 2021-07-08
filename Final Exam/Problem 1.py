9497ca717032e8fda6e5b4ef1ec14097010381eb
Mmorami <46481381+Mmorami@users.noreply.github.com>
2021-07-08 22:27:39 +0300

    Final exam problem 1

---
+++ Final Exam/Problem 1.py
@@ -0,0 +1,21 @@
+# In the statement L = [1,2,3], L is a class.
+# True
+# False [X] - L is of type <list>
+
+# The orders of growth of  and  are both polynomial. -----> the question is corrupted
+# True
+# False 
+
+# The complexity of binary search on a sorted list of n items is . ---> question is corrupted. the true answer is log(n)
+# True
+# False 
+
+# A bisection search algorithm always returns the correct answer when searching for an element in a sorted list.
+# True [X] - If the element is in the list it will be found, and if it is'nt it will say the element is'nt there but the
+# list must be guarenteed to be sorted
+# False
+
+# Performing binary search on an unsorted list will always return the correct 
+# answer in  time where  is the length of the list.
+# True
+# False [X] - if the list i unsorted bisection search has no meaning as we cannot eliminate half the list by a simple inequality
